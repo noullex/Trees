8136dd559e33c2ea91ac379ef860246066ab95b0
Ann <noullex@gmail.com>
2017-01-03 19:25:06 +0300

    binary tree

--- src/com/company/BinarySearchTree.java
+++ src/com/company/BinarySearchTree.java
@@ -9,78 +9,25 @@ public class BinarySearchTree<T extends Comparable> {
     }
 
     public void insert(T value) {
-        root = insert(value, root);
+        root = insert(root, value);
     }
 
-    private Node<T> insert(T value, Node<T> currentNode) {
+    private Node<T> insert(Node<T> currentNode, T value) {
         if (currentNode == null) {
             currentNode = new Node<>(value);
         } else if (value.compareTo(currentNode.data) < 0) {
-            currentNode.left = insert(value, currentNode.left);
+            currentNode.left = insert(currentNode.left, value);
         } else if (value.compareTo(currentNode.data) > 0) {
-            currentNode.right = insert(value, currentNode.right);
+            currentNode.right = insert(currentNode.right, value);
         }
-        return currentNode;
-    }
-
-    public void remove(T value) throws Exception {
-        root = remove(value, root);
-    }
-
-    protected Node<T> remove(T x, Node<T> currentNode) throws Exception {
-        if (currentNode == null) {
-            throw new Exception("Element not found");
-        }
-        if (x.compareTo(currentNode.data) < 0) {
-            currentNode.left = remove(x, currentNode.left);
-        } else if (x.compareTo(currentNode.data) > 0) {
-            currentNode.right = remove(x, currentNode.right);
-        } else if (currentNode.left != null && currentNode.right != null) {
-            currentNode.data = (T) findMin(currentNode.right).data;
-            currentNode.right = removeMin(currentNode.right);
-        } else
-            currentNode = (currentNode.left != null) ? currentNode.left : currentNode.right;
-        return currentNode;
-    }
-
-    private Node<T> removeMin(Node<T> currentNode) {
-        if (currentNode.left != null) {
-            currentNode.left = removeMin(currentNode.left);
-            return currentNode;
-        } else {
-            return currentNode.right;
-        }
-    }
-
-    public Node<T> findMin() {
-        return findMin(root);
-    }
-
-    protected Node<T> findMin(Node<T> currentNode) {
-        if (currentNode != null)
-            while (currentNode.left != null) {
-                currentNode = currentNode.left;
-            }
-        return currentNode;
-    }
-
-    public Node<T> findMax() {
-        return findMax(root);
-    }
-
-    private Node<T> findMax(Node<T> currentNode) {
-        if (currentNode != null)
-            while (currentNode.right != null) {
-                currentNode = currentNode.right;
-            }
         return currentNode;
     }
 
     public Node<T> find(T value) {
-        return find(value, root);
+        return find(root, value);
     }
 
-    private Node<T> find(T value, Node<T> currentNode) {
+    private Node<T> find(Node<T> currentNode, T value) {
         while (currentNode != null) {
             if (value.compareTo(currentNode.data) < 0)
                 currentNode = currentNode.left;
@@ -90,5 +37,35 @@ public class BinarySearchTree<T extends Comparable> {
                 return currentNode;
         }
         return null;
+    }
+
+    public void remove(T value) throws Exception {
+        Node<T> currentNode = find(value);
+        if (currentNode != null) {
+            currentNode = (currentNode.left != null) ? currentNode.left : currentNode.right;
+        } else {
+            throw new Exception("Element not found");
+        }
+    }
+
+
+    public T findMin() {
+        Node<T> currentNode = root;
+        if (currentNode != null) {
+            while (currentNode.left != null) {
+                currentNode = currentNode.left;
+            }
+        }
+        return currentNode.data;
+    }
+
+    public T findMax() {
+        Node<T> currentNode = root;
+        if (currentNode != null) {
+            while (currentNode.right != null) {
+                currentNode = currentNode.right;
+            }
+        }
+        return currentNode.data;
     }
 }
--- src/com/company/BinaryTree.java
+++ src/com/company/BinaryTree.java
@@ -1,4 +1,79 @@
 package com.company;
 
-public class BinaryTree {
+public class BinaryTree<T extends Comparable> {
+
+    private Node<T> root;
+
+    public BinaryTree() {
+        root = null;
+    }
+
+    public void insert(T value) {
+        root = insert(root, value);
+    }
+
+    private Node<T> insert(Node<T> currentNode, T value) {
+        if (currentNode == null)
+            root = new Node<>(value);
+        else {
+            if (currentNode.left == null)
+                currentNode.left = insert(currentNode.left, value);
+            else {
+                currentNode.right = insert(currentNode.left, value);
+            }
+        }
+        return currentNode;
+    }
+
+    public Node<T> find(T value) {
+        return find(root, value);
+    }
+
+    private Node<T> find(Node<T> currentNode, T value) {
+        if (value.equals(currentNode.data)) {
+            return currentNode;
+        } else if (currentNode.left != null) {
+            find(currentNode.left, value);
+        } else if (currentNode.right != null) {
+            find(currentNode.right, value);
+        }
+        return null;
+    }
+
+    public void remove(T value) throws Exception {
+        Node<T> currentNode = find(value);
+        if (currentNode != null) {
+            currentNode = (currentNode.left != null) ? currentNode.left : currentNode.right;
+        } else {
+            throw new Exception("Element not found");
+        }
+    }
+
+    public T findMin() {
+        return findMin(root, root.data);
+    }
+
+    private T findMin(Node<T> currentNode, T minValue) {
+        minValue = minValue.compareTo(currentNode.data) > 0 ? currentNode.data : minValue;
+        if (currentNode.left != null) {
+            findMin(currentNode.left, minValue);
+        } else if (currentNode.right != null) {
+            findMin(currentNode.right, minValue);
+        }
+        return minValue;
+    }
+
+    public T findMax() {
+        return findMin(root, root.data);
+    }
+
+    private T findMax(Node<T> currentNode, T maxValue) {
+        maxValue = maxValue.compareTo(currentNode.data) < 0 ? currentNode.data : maxValue;
+        if (currentNode.left != null) {
+            findMin(currentNode.left, maxValue);
+        } else if (currentNode.right != null) {
+            findMin(currentNode.right, maxValue);
+        }
+        return maxValue;
+    }
 }
