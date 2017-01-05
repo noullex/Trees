f156ff86df91e99b0989141ae56b52d4183f83e1
Ann <noullex@gmail.com>
2017-01-05 16:45:05 +0300

    modify binary tree

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -16,11 +16,18 @@ public class Main {
             bst.insert(i);
             avlt.insert(i);
         }
-        //bt.remove(0);
-        bst.remove(9);
-        avlt.remove(9);
-        //System.out.print(bt.findMin() + "\n");
-        System.out.print(bst.findMax() + "\n");
-        System.out.print(avlt.findMax());
+        final int RM_VALUE = 5;
+        bt.remove(RM_VALUE);
+        bst.remove(RM_VALUE);
+        avlt.remove(RM_VALUE);
+
+        System.out.print("I. TEST MIN/MAX\n");
+        System.out.print("Min and max value in binary tree: " + bt.findMin() + ", " + bt.findMax() + "\n");
+        System.out.print("Max and max value in binary search tree: " + bst.findMin() + ", " + bst.findMax() + "\n");
+        System.out.print("Max and max value in AVL tree: " + avlt.findMin() + ", " + avlt.findMax() + "\n");
+        System.out.print("I. TEST REMOVING\n");
+        System.out.print("Find removed value in binary tree: " + bt.find(RM_VALUE) + "\n");
+        System.out.print("Find removed value in binary search tree: " + bst.find(RM_VALUE) + "\n");
+        System.out.print("Find removed value in AVL tree: " + avlt.find(RM_VALUE));
     }
 }
